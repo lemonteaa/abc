9b38c411a74d9464dd9ba34e37b72b252b2f9f46
Lemontea <lemontea.Tom@gmail.com>
2022-08-11 06:48:16 +0800

    [minor] insert code sample from JSONSchema->malli PR

--- service.clj
+++ service.clj
@@ -21,3 +21,31 @@
     "Ivan")
 
 (.close node)
+
+; Experimental code from PR
+; https://github.com/metosin/malli/pull/211
+; Fork at hkupty:js-schema-to-malli
+(require '[clojure.walk :refer (keywordize-keys)])
+
+(def data {"$id" "https://example.com/person.schema.json",
+           "$schema" "https://json-schema.org/draft/2020-12/schema",
+           "title" "Person",
+           "type" "object",
+           "properties"
+           {"firstName"
+            {"type" "string", "description" "The person's first name."},
+            "lastName"
+            {"type" "string", "description" "The person's last name."},
+            "age"
+            {"description"
+             "Age in years which must be equal to or greater than zero.",
+             "type" "integer",
+             "minimum" 0}}})
+
+(def my-schema (schema->malli (keywordize-keys data)))
+(m/schema? my-schema)
+(m/form my-schema)
+(assert (= true (m/validate my-schema (keywordize-keys {"firstName" "John", "lastName" "Doe", "age" 21}))))
+
+(assert (not= true (m/validate my-schema (keywordize-keys {"firstName" "John", "lastName" 1, "age" 21}))))
+
