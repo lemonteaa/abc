b5c2dc8a1404f4c797aa90fc5c06639f92dbbf1b
lemonteaa <lemontea.Tom@gmail.com>
2022-08-12 21:42:04 +0000

    sample resolver using xtdb

--- src/abc/lacinia.clj
+++ src/abc/lacinia.clj
@@ -8,21 +8,30 @@
             [clojure.java.io :as io]
             [com.walmartlabs.lacinia.util :refer [attach-resolvers]]
             [com.walmartlabs.lacinia.schema :as schema]
-            [com.walmartlabs.lacinia :refer [execute]]))
+            [com.walmartlabs.lacinia :refer [execute]]
+            [xtdb.api :as xt]))
 
 
 (defn get-hero [context arguments value]
-  (let [{:keys [episode]} arguments]
-    (println (:system/db context))
-    (if (= episode :NEWHOPE)
-      {:id 1000
-       :name "Luke"
-       :home_planet "Tatooine"
-       :appears_in ["NEWHOPE" "EMPIRE" "JEDI"]}
-      {:id 2000
-       :name "Lando Calrissian"
-       :home_planet "Socorro"
-       :appears_in ["EMPIRE" "JEDI"]})))
+  (let [{:keys [episode]} arguments
+        dbval (xt/db (:system/db context))]
+    (clojure.set/rename-keys (first (first (xt/q dbval
+          '{:find [(pull ?p [*])]
+            :in [ep-name]
+            :where [[?p :appears_in ep-name]]}
+          (name episode))))
+                             {:xt/id :id})))
+
+;(println (:system/db context))
+;(if (= episode :NEWHOPE)
+;  {:id 1000
+;   :name "Luke"
+;   :home_planet "Tatooine"
+;   :appears_in ["NEWHOPE" "EMPIRE" "JEDI"]}
+;  {:id 2000
+;   :name "Lando Calrissian"
+;   :home_planet "Socorro"
+;   :appears_in ["EMPIRE" "JEDI"]})
 
 (def resolvers {:get-hero get-hero
                 :get-droid (constantly {})})
--- src/abc/service.clj
+++ src/abc/service.clj
@@ -3,6 +3,7 @@
             [malli.core :as m]
             [malli.json-schema.parse :refer [schema->malli]]
             [clojure.walk :refer (keywordize-keys)]
+            [abc.di.components :refer [system]]
             ))
 ; [malli.dev :as mdev]
 ; [malli.experimental :as mx]
@@ -18,6 +19,27 @@
 ;                      :born 1613
 ;                      :remark "A Man of Greatest Talent"}]])
 
+
+(xt/submit-tx
+ (:db/node system) [[::xt/put
+                     {:xt/id 1001
+                      :name "Yoda"
+                      :home_planet "Unknown"
+                      :appears_in ["EMPIRE" "JEDI"]}]])
+(xt/submit-tx
+ (:db/node system) [[::xt/put
+                     {:xt/id 1002
+                      :name "Anakin"
+                      :home_planet "Federation"
+                      :appears_in ["NEWHOPE" "JEDI"]}]])
+(xt/submit-tx
+ (:db/node system) [[::xt/put
+                     {:xt/id 1003
+                      :name "Luke"
+                      :home_planet "Tatooine"
+                      :appears_in ["NEWHOPE" "EMPIRE" "JEDI"]}]])
+
+
 ; https://docs.xtdb.com/language-reference/datalog-queries/
 ;(xt/q
 ; (xt/db node)
@@ -26,6 +48,21 @@
 ;            [p1 :born yr]]
 ;   :in [n]}
 ; :Picasso)
+
+(defn in?
+  "true if coll contains elm"
+  [coll elm]
+  (some #(= elm %) coll))
+
+(first (first (xt/q (xt/db (:db/node system))
+      '{:find [(pull ?p [*])]
+        :in [ep-name]
+        :where [[?p :appears_in ep-name]]}
+      "NEWHOPE")))
+
+(name :NEWHOPE)
+
+;[(abc.service/in? episodes ep-name)]
 
 ;(.close node)
 
